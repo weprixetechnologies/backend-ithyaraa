2acda40b214420f83da6b4b4aa1aedf4b9d53060
Ronit Sarkar <rseditz57@gmail.com>
2025-10-27 13:02:20 +0530

    Server Push

--- utils/dbconnect.js
+++ utils/dbconnect.js
@@ -1,8 +1,9 @@
 const mysql = require('mysql2')
+
 const pool = mysql.createPool({
     host: '127.0.0.1',
-    user: 'root',
-    password: 'rseditz@222',
+    user: 'adminuser',
+    password: 'Vishal@13241',
     database: 'ithyaraa'
 })
 
