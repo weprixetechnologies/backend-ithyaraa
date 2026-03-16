09ed81a0eb69a30e97e19247c7ae9ebc1d37865b
Ronit Sarkar <rseditz57@gmail.com>
2026-03-16 19:29:26 +0530

    l

--- utils/message.js
+++ utils/message.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const twilio = require('twilio')
 
 const accountSid = process.env.TWILIO_ACCOUNT_SID || 'AC0b7dcf4ab84fe14ddb17db27c08b2dcc';
-const authToken = process.env.TWILIO_AUTH_TOKEN || '0fc4b8f368730b8a524d206eae6e8c2c';
+const authToken = process.env.TWILIO_AUTH_TOKEN || '94641cb8dfc00c8596638e27ee8ebd5d';
 const twilioNumber = process.env.TWILIO_PHONE_NUMBER || '+12347203560';
 
 const client = twilio(accountSid, authToken);
