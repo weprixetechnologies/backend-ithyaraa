952cf0e36b484aaf557808a695e5d0dbcc9f989e
Ronit Sarkar <rseditz57@gmail.com>
2026-04-30 14:31:19 +0530

    l

--- router/deliveryExperienceRouter.js
+++ router/deliveryExperienceRouter.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const feedbackController = require('../controllers/deliveryExperienceController');
-const userAuth = require('../middlewares/authMiddleware');
-const adminAuth = require('../middlewares/adminAuthMiddleware');
+const { verifyAccessToken: userAuth } = require('../middleware/authUserMiddleware');
+const { verifyAccessToken: adminAuth } = require('../middleware/authAdminMiddleware');
 
 // Public route for users to submit feedback
 router.post('/submit', userAuth, feedbackController.submitFeedback);
