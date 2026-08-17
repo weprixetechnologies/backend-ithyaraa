99b722a5f7affbaac08d1c356322a97bc1a25fd1
Ronit Sarkar <rseditz57@gmail.com>
2026-08-17 22:50:18 +0530

    l

--- utils/dbconnect.js
+++ utils/dbconnect.js
@@ -1,11 +1,19 @@
 require('dotenv').config();
 const mysql = require('mysql2');
 
+// const pool = mysql.createPool({
+//     host: process.env.DB_HOST || process.env.HOST || '127.0.0.1',
+//     user: process.env.DB_USER || process.env.USER || 'adminuser',
+//     password: process.env.DB_PASSWORD !== undefined ? process.env.DB_PASSWORD : (process.env.PASSWORD !== undefined ? process.env.PASSWORD : 'Vishal@13241'),
+//     database: process.env.DB_NAME || process.env.DATABASE || 'ithyaraa',
+//     timezone: '+05:30'
+// });
+
 const pool = mysql.createPool({
-    host: process.env.DB_HOST || process.env.HOST || '127.0.0.1',
-    user: process.env.DB_USER || process.env.USER || 'adminuser',
-    password: process.env.DB_PASSWORD !== undefined ? process.env.DB_PASSWORD : (process.env.PASSWORD !== undefined ? process.env.PASSWORD : 'Vishal@13241'),
-    database: process.env.DB_NAME || process.env.DATABASE || 'ithyaraa',
+    host: '127.0.0.1',
+    user: 'adminuser',
+    password: 'Vishal@13241',
+    database: 'ithyaraa',
     timezone: '+05:30'
 });
 
