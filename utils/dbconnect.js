db0f6e3898859b272b117b35ab568aaea97b964c
Ronit Sarkar <rseditz57@gmail.com>
2026-08-17 22:07:26 +0530

    l

--- utils/dbconnect.js
+++ utils/dbconnect.js
@@ -1,11 +1,12 @@
-const mysql = require('mysql2')
+require('dotenv').config();
+const mysql = require('mysql2');
 
 const pool = mysql.createPool({
     host: process.env.DB_HOST || '127.0.0.1',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || 'rseditz@222',
+    user: process.env.DB_USER || 'adminuser',
+    password: process.env.DB_PASSWORD !== undefined ? process.env.DB_PASSWORD : 'Vishal@13241',
     database: process.env.DB_NAME || 'ithyaraa',
     timezone: '+05:30'
-})
+});
 
 module.exports = pool.promise();
