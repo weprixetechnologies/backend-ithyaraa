4068eb223d9c834988920e39e295b1fcf1d1beaf
Ronit Sarkar <rseditz57@gmail.com>
2026-09-08 13:23:21 +0530

    l

--- utils/dbconnect.js
+++ utils/dbconnect.js
@@ -9,21 +9,21 @@ const mysql = require('mysql2');
 //     timezone: '+05:30'
 // });
 
-const pool = mysql.createPool({
-    host: '127.0.0.1',
-    user: 'root',
-    password: 'rseditz@222',
-    database: 'ithyaraa',
-    timezone: '+05:30'
-});
-
 // const pool = mysql.createPool({
 //     host: '127.0.0.1',
-//     user: 'adminuser',
-//     password: 'Vishal@13241',
+//     user: 'root',
+//     password: 'rseditz@222',
 //     database: 'ithyaraa',
 //     timezone: '+05:30'
 // });
+
+const pool = mysql.createPool({
+    host: '127.0.0.1',
+    user: 'adminuser',
+    password: 'Vishal@13241',
+    database: 'ithyaraa',
+    timezone: '+05:30'
+});
 
 module.exports = pool.promise();
 
